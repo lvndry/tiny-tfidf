24ce48ad64476381be3c8d0243974ef11efc9f0d
lvndry <lvndry@protonmail.com>
2021-07-04 17:22:28 +0200

    tf-idf v1

--- main.py
+++ main.py
@@ -15,25 +15,23 @@ import pickle
 import re
 import math
 
-ROOT_FOLDER = os.getcwd() + '/stories'
+ROOT_FOLDER = os.getcwd() + '/queries'
+SAMPLE_LENGTH = 0
+DF = {}
+tf_idf = {}
 corpus = []
+processed_corpus = []
 
 nltk.download('stopwords')
 nltk.download('punkt')
 
 
 def init_corpus():
-    file = open(ROOT_FOLDER+"/index.html", 'r')
-    text = file.read().strip()
-    file.close()
-
-    file_name = re.findall('><A HREF="(.*)">', text)
-    file_title = re.findall('<BR><TD> (.*)\n', text)
-
-    for j in range(len(file_name)):
-        corpus.append(
-            (ROOT_FOLDER + "/" + str(file_name[j]), file_title[j])
-        )
+    for filename in os.listdir(ROOT_FOLDER):
+        full_path = os.path.join(ROOT_FOLDER+"/"+filename)
+        corpus.append((full_path, filename))
+    global SAMPLE_LENGTH
+    SAMPLE_LENGTH = len(corpus)
 
 
 def remove_stopwords(story):
@@ -72,25 +70,88 @@ def preprocess(story: str):
     story = remove_useless_chars(story)
     story = stemming(story)
 
-    return word_tokenize(story)
+    return story
 
 
-processed_corpus = []
-processed_titles = []
+def words_document_frequency():
+    for i in range(SAMPLE_LENGTH):
+        vocab = processed_corpus[i]
+        for w in vocab:
+            try:
+                DF[w].add(i)
+            except:
+                DF[w] = {i}
+
+    for i in DF:
+        DF[i] = len(DF[i])
 
 
-def extract_docs(corpus):
-    sample = corpus[:10]
+def get_word_df(word):
+    df = 0
+    try:
+        df = DF[word]
+    except:
+        pass
+    return df
+
+
+def get_vocab_tfidf():
+    for i in range(SAMPLE_LENGTH):
+        story_words = processed_corpus[i]
+        counter = Counter(story_words)
+        words_count = len(story_words)
+
+        for word in np.unique(story_words):
+            tf = counter[word]/words_count
+            df = get_word_df(word)
+            idf = np.log((SAMPLE_LENGTH+1)/(df+1))
+
+            tf_idf[(i, word)] = tf*idf
+
+
+def extract_docs_terms(corpus):
+    sample = corpus[:SAMPLE_LENGTH]
     for path, title in sample:
         file = open(path, 'r', encoding="utf8", errors='ignore')
         text = file.read().strip()
         file.close()
-        text = preprocess(text)
-        title = preprocess(title)
+        text = word_tokenize(preprocess(text))
         processed_corpus.append(text)
-        processed_titles.append(title)
-    print(processed_titles)
+
+
+def search_most_relevant_story(query):
+    tokens = word_tokenize(preprocess(query))
+
+    print("Query:", query)
+    print(tokens)
+
+    query_weights = {}
+
+    for tfidf in tf_idf:
+        doc_index, word = tfidf
+        if word in tokens:
+            try:
+                query_weights[doc_index] += tf_idf[tfidf]
+            except:
+                query_weights[doc_index] = tf_idf[tfidf]
+
+    query_weights = sorted(query_weights.items(),
+                           key=lambda x: x[1], reverse=True)
+
+    results = []
+
+    for index, score in query_weights[:10]:
+        path, _ = corpus[index]
+        results.append((path, score))
+
+    f = open(results[0][0], 'r')
+    text = f.read().strip()
+    f.close()
+    print(text)
 
 
 init_corpus()
-extract_docs(corpus)
+extract_docs_terms(corpus)
+words_document_frequency()
+get_vocab_tfidf()
+search_most_relevant_story("best subject")
